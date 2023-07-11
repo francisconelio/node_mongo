1fb4fdb7e50defacdbea4d0c13a387f514e3d149
Francisco Nélio <nelio.romero.fn@gmail.com>
2023-07-10 21:51:04 -0300

    node_mongo

--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
-const express = require('express')
-const nodemon = require('nodemon')
+//const express = require('express')
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
 const app = express()
 
 const HOST = 'localhost'
 const port = 5000
 
+// endpoint inicial
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+
+console.log(DB_USER,DB_PASS)
+
+//Conectando no banco Mongo Atlas
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.qprbpqk.mongodb.net/usuario?retryWrites=true&w=majority`)
+.then(()=>{
+    console.log('BD conectado com sucesso')
+})
+.catch(error => {
+    console.log('Erro ao conectar no BD', error)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port http://${HOST}:${port}`)
 })
