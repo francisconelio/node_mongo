2d0124d9050d763c5888441c4786fbf76c243124
Francisco Nélio <nelio.romero.fn@gmail.com>
2023-07-31 21:33:48 -0300

    validações

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -9,8 +9,21 @@ class UsuarioController {
         //desestrutruração do body para acessar os atributos
         const { nome, idade, ativo, email } = req.body;
 
+        
         // crio o ojeto usuario copiado do objeto body
         const usuario = { nome, idade, ativo, email }
+
+        Object.keys(usuario).map(key =>{console.log('chave:',key,'valor:', usuario[key])
+
+        if(usuario[key]) {
+            console.log('validar');
+            res.status(422).json(`o campo ${key}, é vazio`);
+            //interrompe a criação, ou seja, abaixo dele não executa a inserção
+            return;
+        }
+
+        })
+
 
         //cria o usuario atraves do mongoose
         const usuarioDB = await Usuario.create(usuario)
@@ -29,8 +42,13 @@ class UsuarioController {
         // crio o ojeto usuario copiado do objeto body
         const usuario = { nome, idade, ativo, email }
 
-        await Usuario.updateOne({ _id: id }, usuario)
-        res.status(204).json('Usuário atualizado com sucesso!')
+       const updateUsuario = await Usuario.updateOne({ _id: id }, usuario)
+       console.log(updateUsuario)
+
+       if(updateUsuario.matchedCount === 0) {
+        res.status(422).json(`Usuário ${usuario.nome} não foi atualizado!`)
+    }
+    res.status(204).json('Usuário atualizado com sucesso!')
     };
 
     // Buscar usuários
@@ -42,10 +60,27 @@ class UsuarioController {
     //buscar por id
     static buscarPorId = async (req, res) => {
         const id = req.params.id
-        const usuario = await Usuario.findOne({ _id: id })
-        res.status(200).json(usuario)
 
+        if(id.length<24 || id.length>24) {
+            res.status(422).json({message: 'Tamanho inváido do Id!'})
+            return
+        }
+        const usuario = await Usuario.findOne({ _id: id });
+
+        if(!usuarioDB){
+            res.status(422).json({message: "Usuário não encontrado!"});
+            return;
+        }
+
+        console.log(usuario)
+
+        if(!usuario){
+      
+        res.status(422).json({message: 'Usuário não encontrado'})
+        return
     }
+    res.status(200).json(usuario);
+    };
 
 
     static deletarUsuario = async (req, res) => {
@@ -61,10 +96,10 @@ class UsuarioController {
         const id = req.params.id
 
         //Busca o usuário, antes de deletar, com as suas informações
-        const usuarioDB = await Usuario.findOne({ _id: id })
+        const usuarioDB = await Usuario.findOne({ _id: id });
 
         // Deleta o usuário do banco
-        await Usuario.deleteOne({ _id: usuarioDB.id })
+        await Usuario.deleteOne({ _id: usuarioDB.id });
 
         //Pego a data atual, new Date(), e formato
         let date = moment(new Date()).format("DD/MM/YYYY hh:mm:ss");
